d4d4f2dbef23002b648be31ffd125d27c36dc267
GregoriSimei <40555037+GregoriSimei@users.noreply.github.com>
2020-10-05 20:28:25 -0300

    configuracoes do client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListFarmaComponent } from './views/farma/list-farma/list-farma.component';
 import { CreateFarmaComponent } from './views/farma/create-farma/create-farma.component';
+import { ListProdutoComponent } from './views/prod/list-produto/list-produto.component';
+import { CreateProdutoComponent } from './views/prod/create-produto/create-produto.component';
+import { UpdateProdutoComponent } from './views/prod/update-produto/update-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListFarmaComponent,
-    CreateFarmaComponent
+    CreateFarmaComponent,
+    ListProdutoComponent,
+    CreateProdutoComponent,
+    UpdateProdutoComponent
   ],
   imports: [
     BrowserModule,
